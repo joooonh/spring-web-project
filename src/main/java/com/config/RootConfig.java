65a7764c3de3c8ae934a905effa2f63a09beeb53
joooonh <joooonh12@gmail.com>
2023-10-03 19:15:08 +0900

    스프링과 oracle database 연동

--- src/main/java/com/config/RootConfig.java
+++ src/main/java/com/config/RootConfig.java
@@ -1,10 +1,28 @@
 package com.config;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan(basePackages = {"com.sample"})
 public class RootConfig {
+
+    // HikariDataSource를 스프링 빈으로 등록
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+        hikariConfig.setJdbcUrl("jdbc:oracle:thin:@localhost:1521:XE");
+        hikariConfig.setUsername("book_ex");
+        hikariConfig.setPassword("book_ex");
+
+        HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+        return dataSource;
+    }
 
 }
--- src/main/java/com/sample/Chef.java
+++ src/main/java/com/sample/Chef.java
@@ -7,5 +7,4 @@ import org.springframework.stereotype.Component;
 @Data
 public class Chef {
 
-
 }
