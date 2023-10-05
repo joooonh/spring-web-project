6a217b7624d26c84ca0734789810911e227a4309
joooonh <joooonh12@gmail.com>
2023-10-05 13:48:42 +0900

    스프링 MVC 프로젝트의 기본 구성

--- src/main/java/com/example/config/RootConfig.java
+++ src/main/java/com/example/config/RootConfig.java
@@ -1,7 +1,39 @@
 package com.example.config;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
+@MapperScan(basePackages = {"com.example.mapper"})
 public class RootConfig {
+
+    // HikariDataSource를 스프링 빈으로 등록
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setDriverClassName("net.sf.log4jdbc.sql.jdbcapi.DriverSpy");
+        hikariConfig.setJdbcUrl("jdbc:log4jdbc:oracle:thin:@localhost:1521:XE");
+
+        hikariConfig.setUsername("book_ex");
+        hikariConfig.setPassword("book_ex");
+
+        HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+
+        return dataSource;
+    }
+
+    // SqlSessionFactory를 스프링 빈으로 등록
+    @Bean
+    public SqlSessionFactory sqlSessionFactory() throws Exception {
+        SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
+        sqlSessionFactory.setDataSource(dataSource());
+        return sqlSessionFactory.getObject();
+    }
 }
--- src/main/java/com/example/config/ServletConfig.java
+++ src/main/java/com/example/config/ServletConfig.java
@@ -2,13 +2,14 @@ package com.example.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 @EnableWebMvc
-@ComponentScan(basePackages = {"com.example.controller", "com.example.exception"})
+@ComponentScan(basePackages = {"com.example.controller"})
 public class ServletConfig implements WebMvcConfigurer {
 
     @Override
@@ -21,4 +22,8 @@ public class ServletConfig implements WebMvcConfigurer {
         registry.viewResolver(bean);
     }
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
 }
