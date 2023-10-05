bae56393f02adb367240b0796d17939b6b51243d
joooonh <joooonh12@gmail.com>
2023-10-05 17:53:15 +0900

    영속 계층 CRUD

--- src/main/java/com/example/mapper/BoardMapper.java
+++ src/main/java/com/example/mapper/BoardMapper.java
@@ -7,4 +7,14 @@ import java.util.List;
 public interface BoardMapper {
 
     public List<BoardVO> getList();
+
+    public void insert(BoardVO boardVO);
+
+    public void insertSelectKey(BoardVO boardVO);
+
+    public BoardVO read(Long bno);
+
+    public int delete(Long bno);
+
+    public int update(BoardVO boardVO);
 }
