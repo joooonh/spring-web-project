9cc19a76cef465f6352a94c6585e2e289ab6112f
joooonh <joooonh12@gmail.com>
2023-10-05 18:26:36 +0900

    비즈니스 계층 CRUD

---
+++ src/main/java/com/example/service/BoardService.java
@@ -0,0 +1,18 @@
+package com.example.service;
+
+import com.example.domain.BoardVO;
+
+import java.util.List;
+
+public interface BoardService {
+
+    public void register(BoardVO board);
+
+    public BoardVO get(Long bno);
+
+    public boolean modify(BoardVO board);
+
+    public boolean remove(Long bno);
+
+    public List<BoardVO> getList();
+}
